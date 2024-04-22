0868f1a74c7dedb5a19ef67939da7cb441b4f908
Hoang <hoangtroll14354@gmail.com>
2024-04-22 20:28:11 +0700

    update

--- client/server/src/routes/BookRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const BookControllers=require('../controllers/BookController')
-const router = express.Router();
-router.post('/create-book',BookControllers.createBook)
-router.get('/getAllBook',BookControllers.getAllBook)
-router.put('/update-book/:id',BookControllers.updateBook)
-router.delete('/delete-book/:id',BookControllers.deleteBook)
-router.get('/getDetailBook/:id',BookControllers.getDetailsBook)
-module.exports=router
